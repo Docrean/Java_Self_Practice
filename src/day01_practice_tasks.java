861f9f6a0871cbd3a2bf626d29cbdf946eb389f0
DocBFxClass <bverns88@gmail.com>
2024-02-04 09:23:50 -0800

    Day1UPDATE

--- src/day01_practice_tasks.java
+++ src/day01_practice_tasks.java
@@ -2,8 +2,53 @@ public class day01_practice_tasks {
 
     public static void main(String[] args) {
 
+        System.out.println("Hello");
+        System.out.println("World");
 
-            }
-        }
+        System.out.println("-------------------------------");
+
+        System.out.print("Hello");
+        System.out.print("World");
+        System.out.println(); //append new line after last System.out.print statement
+        System.out.println("-------------------------------");
+
+        System.out.print("Hello ");
+        System.out.println("Cydeo");
+        System.out.print("I love ");
+        System.out.println("Programming.");
+
+        System.out.println("-----------------------------");
+
+        System.err.println("doogies:");
+        System.err.println("; expected");
+
+        //This is the way
+
+        /*
+        This is the way
+        to multi-line comment
+         */
+
+        /*
+        \n Newline Moves the cursor to the next line for subsequent printing
+        \t Horizontal Tab Moves the cursor to skip over to the next tab stop
+        \\ Backslash Causes a backslash to be printed
+        \" Double quote Causes a double quotation mark to be printed
+        \' Single quote Causes a single quotation mark to be printed (used in char data type)
+        \b Backspace Moves the cursor to back up, or move left, one position
+        \r Return Moves the cursor to the beginning of the current line, not the next line
+         */
+
+        String slash = "\"__/";
+        String word = "Hello";
+        String coffee ="\'coffee\'";
+        String tea ="\'tea\'";
+
+        System.out.println(slash);
+        System.out.println("He said " + word + ".");
+        System.out.println("Would you like " + coffee + " or " + tea + "?");
 
 
+
+    }
+}
