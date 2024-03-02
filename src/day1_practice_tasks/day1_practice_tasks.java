a55f8c1bda560b66c8bbb34da4d75aee974db222
DocBFxClass <bverns88@gmail.com>
2024-03-01 20:06:27 -0800

    practiceTasksUpdate

---
+++ src/day1_practice_tasks/day1_practice_tasks.java
@@ -0,0 +1,108 @@
+package day1_practice_tasks;
+
+public class day1_practice_tasks {
+
+    public static void main(String[] args) {
+
+
+        //JavaComponents
+
+        System.out.println("JDK: Java Development Kit");
+        System.out.println("JRE: Java Runtime Environment");
+        System.out.println("JVA: Java Virtual Machine");
+
+        System.out.println("------------------------------------");
+
+        //PrintExecution
+
+        System.out.println("Cydeo");
+        System.out.println();
+        System.out.println("Yulia");
+        System.out.println("Alena");
+        System.out.println("Lucy");
+        System.out.println("Alexander");
+        System.out.println();
+        System.out.println("Igor");
+        System.out.println("Sumeye");
+        System.out.println("Madina");
+
+        System.out.println("------------------------------------");
+
+        //GasReceipt
+
+        System.out.println("   MCLEAN STORE   ");
+        System.out.println();
+        System.out.println("04-29-2022   02:45 pm");
+        System.out.println();
+        System.out.println("Gallons:          11.48");
+        System.out.println("Price/gallon:    $4.15");
+        System.out.println();
+        System.out.println("Fuel total:      $47.642");
+
+        System.out.println("------------------------------------");
+
+        //Diamond1
+
+        System.out.println("\t          ^        ");
+        System.out.println("\t        /  \\      ");
+        System.out.println("\t       /    \\     ");
+        System.out.println("\t      /      \\    ");
+        System.out.println("\t     /        \\   ");
+        System.out.println("\t     ----------    ");
+        System.out.println("\t     \\        /   ");
+        System.out.println("\t      \\      /    ");
+        System.out.println("\t       \\    /     ");
+        System.out.println("\t        \\  /      ");
+        System.out.println("\t         \\/       ");
+
+        System.out.println("------------------------------------");
+
+        //Diamond2
+
+        System.out.println("\t         ^       ");
+        System.out.println("\t        / \\      ");
+        System.out.println("\t       / * \\     ");
+        System.out.println("\t      / * * \\    ");
+        System.out.println("\t     / * * * \\   ");
+        System.out.println("\t     ---------    ");
+        System.out.println("\t     \\ * * * /   ");
+        System.out.println("\t      \\ * * /    ");
+        System.out.println("\t       \\ * /     ");
+        System.out.println("\t        \\ /      ");
+        System.out.println("\t         V       ");
+
+        System.out.println("------------------------------------");
+
+        //AboutMe
+
+        /*
+    Create a class named AboutMe and write a code to print the following info about you:
+    Your favorite Book
+    Your favorite quote
+    Your Favorite TV Show
+    Your Favorite music
+
+    Nte: Note: Make sure the titles and quotes are printed within double quotes on the console.
+    */
+        System.out.println("My favorite book is \"The bible\"\n\tMy Favorite quote is \"Secut enum homo cogitat ita est\"\n" +
+                "\tMy favorite TV Show is \"Scooby Doo\"\n\tMy favorite music is \"Holy\"");
+
+        System.out.println("Welcome to the patient portal!\nPlease enter your personal information");
+
+        System.out.println("------------------------------------");
+
+        //Triangle
+
+        System.out.println("\t         ^    ");
+        System.out.println("\t        / \\    ");
+        System.out.println("\t       /   \\    ");
+        System.out.println("\t      /     \\   ");
+        System.out.println("\t     /       \\   ");
+        System.out.println("\t     ---------    ");
+
+
+
+
+    }
+}
+
